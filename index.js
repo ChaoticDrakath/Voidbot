86f7992c99987e7507225e81ed081ea93e409978
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-08-01 23:37:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.once('ready', () => {
     client.user.setActivity("Rules!", {type: "STREAMING"});
 })
 
-client.on('guildMemberAdd', (guildMember) => {
+client.on('guildMemberAdd', (guildMember)) => {
    guildMember.addRole(guildMember.guild.roles.find(role => role.name === "member"));
 }
 
