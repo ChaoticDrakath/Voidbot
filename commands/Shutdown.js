fb6729c376190be1b74a70307a9a64242af7f325
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-08-01 23:57:37 +0300

    Update Shutdown.js

--- commands/Shutdown.js
+++ commands/Shutdown.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if(message.author.id = "471988330335174667") return message.channel.send("You're my commander!")
+    if(message.author.id = "471988330335174667") {
     
     try {
         await message.channel.send("Voidguard is going offduty!")
@@ -11,6 +11,7 @@ module.exports.run = async (client, message, args) => {
         message.channel.send("ERROR: ${e.message}")
     }
     
+}
 }
 
 module.exports.help = {
