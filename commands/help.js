0e745d9136780997830d1c5f74dc38fde8c50322
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-09-14 18:39:37 +0300

    Update and rename uac.js to help.js

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    let bicon = client.user.displayAvatarURL;
+    let useracommands = new Discord.RichEmbed()
+    .setAuthor('Help', 'https://i.imgur.com/NXWBmum.png')
+    .setThumbnail(bicon)
+    .setDescription("All commands are listed below.")
+    .setColor("#980aab")
+    .addField("User accessible commands", "Vbotinfo - info of about VoidGuard\nVserverinfo - info about this server\nVhelp - gives help")
+
+    return message.channel.send(useracommands);
+   }
+
+module.exports.help = {
+    name: "help"
+}
