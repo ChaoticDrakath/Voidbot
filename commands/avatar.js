e801d18e721a11d324b818f0e70c9215be09c62f
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-09-21 09:31:29 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,6 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+		});
+
+		message.channel.send(avatarList);
+	};
     
 
 module.exports.help = {
