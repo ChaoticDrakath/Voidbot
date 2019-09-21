4dbbf1b6676b036870f5895187ffd43ff2a2c561
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-09-21 18:35:14 +0300

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
 
     if(message.member.hasPermission("MANAGE_MESSAGES")) {
     message.member.setNickname(message.content.replace('changeNick ', ''));
-}
+}}
 
 module.exports.help = {
     name: "nick"
