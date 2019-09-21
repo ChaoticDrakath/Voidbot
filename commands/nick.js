c9694d41780b6073014d4865d4451001183b0106
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-09-21 21:30:23 +0300

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     if(message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.member.setNickname(message.content.replace('changeNick ', ''));
+    message.mentions.members.first.setNickname(message.content.replace('changeNick ', ''));
 }}
 
 module.exports.help = {
