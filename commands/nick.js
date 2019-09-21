0f5114004827fe51cfff7c7502612e625bae11ed
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-09-21 21:59:09 +0300

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -3,7 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     if(message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.mentions.members.first.setNickname(message.content.replace('changeNick ', ''));
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+        
+        var userID = args[0].replace('<@', '').replace('>', '').replace('!', '');
+        message.channel.send(userID);
+        message.guild.members.get(userID).setNickname("test", "nick command executed");
+        message.channel.send(`Successfully changed ${args[0]}'s nickname to "${message.channel.server.detailsOf(args[0]).nick}"`);
+    }
+});
 }}
 
 module.exports.help = {
