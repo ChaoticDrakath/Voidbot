e4b2192345c44d90d1961c19f210686ccd9abe33
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-09-21 18:32:31 +0300

    Create nick.js

---
+++ commands/nick.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    if(message.member.hasPermission("MANAGE_MESSAGES")) {
+   
+}
+
+module.exports.help = {
+    name: "nick"
+}
