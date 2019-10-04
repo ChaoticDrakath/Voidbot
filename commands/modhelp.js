33ff1df6080987805aaa96cbb815f75f17b02f56
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-04 19:34:49 +0300

    Update modhelp.js

--- commands/modhelp.js
+++ commands/modhelp.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     let moderatoracommands = new Discord.RichEmbed()
     .setAuthor('moderator accessible Commands', 'https://i.imgur.com/NXWBmum.png')
     .setThumbnail(bicon)
-    .setDescription("This command shows Voidguard's moderator commands.")
+    .setDescription("This command shows Fuyu's moderator commands.")
     .setColor("#980aab")
     .addField("moderator commands:", "Vban [@user] - bans mentioned user!\nVsay [#channel] [message] - Administrator only command!(mentioning channel is optional)\nVclear [amount] - clears chat\nVkick [@user] - kicks mentioned user!\nVbotinfo - info of about VoidGuard\nVserverinfo - info about this server\nVhelp - shows all usable commands")
 
