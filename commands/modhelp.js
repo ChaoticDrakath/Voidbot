c77f00269a924b10814a0cadb7d7601d4d3b2363
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-05 16:02:06 +0300

    Update modhelp.js

--- commands/modhelp.js
+++ commands/modhelp.js
@@ -5,11 +5,10 @@ module.exports.run = async (client, message, args) => {
 
     let bicon = client.user.displayAvatarURL;
     let moderatoracommands = new Discord.RichEmbed()
-    .setAuthor('moderator accessible Commands', 'https://i.imgur.com/NXWBmum.png')
+    .setAuthor('moderation Commands', 'https://i.imgur.com/NXWBmum.png')
     .setThumbnail(bicon)
-    .setDescription("This command shows Fuyu's moderator commands.")
     .setColor("#980aab")
-    .addField("moderator commands:", "Vban [@user] - bans mentioned user!\n\nVmute [@user] [time] [reason] - mutes mentioned user!\n\nVdm [message] - sends DM to all server members. (Admin only)\n\nVsay [#channel] [message] - Sends message to channel(mentioning channel is optional(admin only))\n\nVclear [amount] - clears chat\n\nVkick [@user] - kicks mentioned user!\n\nVbotinfo - info of about this bot\nVserverinfo - info about this server\n\nVhelp - shows all usable commands")
+    .addField("moderation commands:", "Vban [@user] - bans mentioned user!\n\nVmute [@user] [time] [reason] - mutes mentioned user!\n\nVdm [message] - sends DM to all server members. (Admin only)\n\nVsay [#channel] [message] - Sends message to channel(mentioning channel is optional(admin only))\n\nVclear [amount] - clears chat\n\nVkick [@user] - kicks mentioned user!\n\nVbotinfo - info of about this bot\nVserverinfo - info about this server\n\nVhelp - shows all usable commands")
 
     return message.channel.send(moderatoracommands);
    }else message.channel.send("u aren't powerful enough to access this area!")
