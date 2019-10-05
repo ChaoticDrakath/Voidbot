8aa0735be09e09a49497f5fa812ae49fb9c47683
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-05 23:12:02 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
     .setThumbnail(bicon)
     .setColor("#980aab")
     .addField("Commands:", "Vbotinfo - info of about this bot\n\nVreport [@user] [reason] - reports mentioned user to mods with provided reason\n\nVavatar - shows mentioned user avatar.\n\nVserverinfo - info about this server\n\nVhelp - gives help", true)
-    .addField("Moderation Commands:", "Vban [@user] - bans mentioned user!\n\nVuserinfo [@user] - shows mentioned user info (mentioning is optional)\n\nVmute [@user] [time] [reason] - mutes mentioned user!\n\nVdm [message] - sends DM to all server members. (Admin only)\n\nVsay [#channel] [message] - Sends message to channel(mentioning channel is optional)\n\nVclear [amount] - clears chat\n\nVkick [@user] - kicks mentioned user!\n\nVbotinfo - info of about this bot\n\nVserverinfo - info about this server", true)
+    .addField("Moderation Commands:", "Vban [@user] - bans mentioned user!\n\nVuserinfo [@user] - shows mentioned user info (mentioning is optional)\n\nVmute [@user] [time] [reason] - mutes mentioned user!\n\nVdm [message] - sends DM to all server members. (Admin only)\n\nVsay [#channel] [message] - Sends message to channel(mentioning channel is optional)\n\nVclear [amount] - clears chat\n\nVkick [@user] - kicks mentioned user!\n\nVserverinfo - info about this server", true)
 
     return message.channel.send(useracommands);
    }
