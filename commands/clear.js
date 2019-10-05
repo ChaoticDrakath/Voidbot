8354fffab4274f1e92f9ef98262bc6019ce7e06e
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-05 22:59:41 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
     if(args[0])
     message.channel.bulkDelete(args[0]).then(() => {
         message.channel.send(`cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-    })}
+    })}else message .channel.send("You don't have required permissions to access this command!");
 }
 
 module.exports.help = {
