70e4cea73fbe71f089fc39629ef1d77e8b6202b2
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-05 15:59:00 +0300

    Update modhelp.js

--- commands/modhelp.js
+++ commands/modhelp.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
     .setThumbnail(bicon)
     .setDescription("This command shows Fuyu's moderator commands.")
     .setColor("#980aab")
-    .addField("moderator commands:", "Vban [@user] - bans mentioned user!\n\nVmute [@user] [time] [reason] - mutes mentioned user!\nVdm [message] - sends DM to all server members. (Admin only)\nVsay [#channel] [message] - Sends message to channel(mentioning channel is optional(admin only))\nVclear [amount] - clears chat\nVkick [@user] - kicks mentioned user!\nVbotinfo - info of about this bot\nVserverinfo - info about this server\nVhelp - shows all usable commands")
+    .addField("moderator commands:", "Vban [@user] - bans mentioned user!\n\nVmute [@user] [time] [reason] - mutes mentioned user!\n\nVdm [message] - sends DM to all server members. (Admin only)\n\nVsay [#channel] [message] - Sends message to channel(mentioning channel is optional(admin only))\n\nVclear [amount] - clears chat\n\nVkick [@user] - kicks mentioned user!\n\nVbotinfo - info of about this bot\nVserverinfo - info about this server\n\nVhelp - shows all usable commands")
 
     return message.channel.send(moderatoracommands);
    }else message.channel.send("u aren't powerful enough to access this area!")
