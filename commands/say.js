2eaa921636a1df2d5a989975bf656e49c5538c39
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-05 12:29:44 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    if(message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])){
+    if(message.member.hasPermission(["SEND_MESSAGES"])){
 
     let argsresult;
     let mChannel = message.mentions.channels.first()
