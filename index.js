0078195a56a5debeae2ee994728b6ef65a68a202
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-05 13:33:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ client.once('ready', () => {
 client.on('message', message => {
     console.log(`${message.author.tag}(ID:${message.author.id}): ${message.content}`);
     if (!message.content.startsWith(prefix)) return;
-    if (!message.content.toLowerCase(prefix)) return;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
